e30c67baa8b9df6582a5b7d4249310c4875c7520
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-05 12:59:26 +0530

    components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 import Image from 'next/image'
 import data  from "@/data.json"
+// import { get } from '@vercel/edge-config';
+import { redirect } from 'next/navigation';
+import LinkCard from './components/LinkCard';
+import TwitterIcon from './components/TwitterIcon';
+import GithubIcon from './components/GithubIcon';
 
-export default function Home() {
+interface Data {
+  name: string;
+  avatar: string;
+  links: Link[];
+}
+interface Link {
+  href: string;
+  title: string;
+  image?: string;
+}
+
+export default async function Home() {
+
+  // const data: Data | undefined = await get('linktree');
+  // const data = null
+
+  if (!data) {
+    // not working yet https://github.com/vercel/next.js/issues/44232
+    redirect('https://linktr.ee/selenagomez');
+  }
+
   return (
     <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
-
-
     <Image
       priority
       className="rounded-full"
@@ -14,8 +37,8 @@ export default function Home() {
       width={96}
       height={96}
     />
-    <h1 className="font-bold mt-4 mb-8 text-xl text-black">{data.name}</h1>
-    {/* {data.links.map((link) => (
+    <h1 className="font-bold mt-4 mb-8 text-xl text-white">{data.name}</h1>
+    {data.links.map((link) => (
       <LinkCard key={link.href} {...link} />
     ))}
     <div className="flex items-center gap-4 mt-8 text-white">
@@ -27,14 +50,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {social.href.includes('twitter') ? (
+          {social.title.includes('twitter') ? (
             <TwitterIcon />
-          ) : social.href.includes('github') ? (
-            <GitHubIcon />
+          ) : social.title.includes('github') ? (
+            <GithubIcon />
           ) : null}
         </a>
       ))}
-    </div> */}
+    </div>
   </div>
   )
 }
