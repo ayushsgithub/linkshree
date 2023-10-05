8157e3e18d3d799190dd53c7042cee34da1aa57e
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-05 13:09:27 +0530

    added links in data

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["pbs.twimg.com", "cdn-icons-png.flaticon.com", "static.dezeen.com"]
+        domains: ["pbs.twimg.com", "cdn-icons-png.flaticon.com", "static.dezeen.com", "encrypted-tbn0.gstatic.com", "cdn.hashnode.com"]
     }
 }
 
